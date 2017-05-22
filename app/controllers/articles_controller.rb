b39bc73964259be0ea30c39747fcd4ab51b8bd8c
Timur Platonov <S.R.mmlacc@outlook.com>
2017-05-22 18:29:52 +0300

    add /admin page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,38 @@
 class ArticlesController < ApplicationController
+  before_action :set_custom_or_new_article, only: %i(edit update)
+
   def last_active
     @article = Article.last_active
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new article_params
+
+    if @article.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_custom_or_new_article
+    @article = (Article.custom || Article.new)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :annotation, :expired_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   root 'articles#last_active'
+  get '/admin', to: 'articles#edit'
+  post '/admin', to: 'articles#create'
+  patch '/admin', to: 'articles#update'
 
   mount Sidekiq::Web => '/sidekiq'
 end
