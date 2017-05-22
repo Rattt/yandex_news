cc0d19f1847596b0d42e9b3e7d235f36c2caf511
Timur Platonov <S.R.mmlacc@outlook.com>
2017-05-22 22:34:01 +0300

    add much more tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get last_active' do
+  test 'should get last_active article' do
     get root_url
     assert_response :success
+  end
+
+  test 'should create custom article' do
+    assert_difference('Article.count') do
+      article_params = { article: attributes_for(:custom_article) }
+      post admin_url, params: article_params
+    end
+
+    assert_redirected_to root_url
+  end
+
+  test 'should update custom article' do
+    article = create(:custom_article)
+    new_article_params = {
+      article: attributes_for(:custom_article, expired_at: 20.minutes.from_now)
+    }
+    patch admin_url, params: new_article_params
+    assert article.reload.expired_at < new_article_params[:article][:expired_at]
+    assert_redirected_to root_url
   end
 end
--- test/jobs/yandex_article_download_job_test.rb
+++ test/jobs/yandex_article_download_job_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class YandexArticleDownloadJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create new yandex article' do
+    assert_difference('Article.count') do
+      YandexArticleDownloadJob.perform_now
+    end
+  end
 end
